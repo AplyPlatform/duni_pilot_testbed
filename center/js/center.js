9a343712790aaf28454255ab5152b460834832fd
gunman <gunman97@gmail.com>
2020-08-16 21:16:38 +0900

    데이터 카운트 다르게 표시

--- center/js/center.js
+++ center/js/center.js
@@ -156,6 +156,10 @@ function setDashBoard(rcount, fcount) {
 			rcount = 1;
 			fcount = 1;
 		}
+		else {
+			$("#r_count_label").text(label_1 + " : " + rcount);
+			$("#f_count_label").text(label_2 + " : " + fcount);
+		}
 		// Set new default font family and font color to mimic Bootstrap's default styling
 		Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 		Chart.defaults.global.defaultFontColor = '#858796';
