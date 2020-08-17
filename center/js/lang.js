31703c5d1381edaefc43d3eb4f8084e36ed054f2
gunman <gunman97@gmail.com>
2020-08-17 13:36:19 +0900

    문법오류

--- center/js/lang.js
+++ center/js/lang.js
@@ -6,11 +6,11 @@ var LANG_JSON_DATA = {
 
 				"data_count_msg" : "님의 데이터 현황",
 				"center_about_title" : "이곳에 관하여...",
-				"center_about_content" : "<p>드론의 비행계획을 쉽게 디자인하고 비행기록과 상황를 확인할 수 있는 곳입니다.</p>
-																	<p>디자인 한 비행계획은 DUNI앱과 DJI제품의 드론에서 사용 할 수 있습니다. DUNI앱은 현재 개발중이며 곧 안드로이드/iOS버전이 배포될 예정입니다. ('비행계획' > '계획 설계' 메뉴)</p>
-																	<p>DUNI앱의 안드로이드 버전은 소스코드가 공개되어 있습니다. <a href='http://code.duni.io/' target=_new>이곳</a>에서 확인할 수 있습니다.</p>
-																	<p>DJI 비행기록 파일과 드론 영상이 있는 유튜브 URL을 업로드하면 자세한 비행기록을 확인할 수 있습니다. ('비행기록' > '기록 업로드' 메뉴)</p>
-																	<p>상단의 <i class='fa fa-user fa-fw'></i> 아이콘을 클릭하면 본인의 개발자 Token을 확인할 수 있습니다.</p>",
+				"center_about_content" : "<p>드론의 비행계획을 쉽게 디자인하고 비행기록과 상황를 확인할 수 있는 곳입니다.</p>" +
+																	"<p>디자인 한 비행계획은 DUNI앱과 DJI제품의 드론에서 사용 할 수 있습니다. DUNI앱은 현재 개발중이며 곧 안드로이드/iOS버전이 배포될 예정입니다. ('비행계획' > '계획 설계' 메뉴)</p>" +
+																	"<p>DUNI앱의 안드로이드 버전은 소스코드가 공개되어 있습니다. <a href='http://code.duni.io/' target=_new>이곳</a>에서 확인할 수 있습니다.</p>" +
+																	"<p>DJI 비행기록 파일과 드론 영상이 있는 유튜브 URL을 업로드하면 자세한 비행기록을 확인할 수 있습니다. ('비행기록' > '기록 업로드' 메뉴)</p>" +
+																	"<p>상단의 <i class='fa fa-user fa-fw'></i> 아이콘을 클릭하면 본인의 개발자 Token을 확인할 수 있습니다.</p>",
 				"msg_no_mission" : "비행계획이 존재하지 않습니다.",
 				 "msg_error_sorry" : "오류가 발생했습니다. 죄송합니다. 잠시후 다시 시도해 주세요.",
 				"msg_no_flight_record" : "존재하는 비행기록이 없습니다.",
@@ -43,11 +43,11 @@ var LANG_JSON_DATA = {
 				 "memu_flight_record" : "Flight record"
 				 "data_count_msg" : "'s data status",
 				 "center_about_title" : "이곳에 관하여...",
-				 "center_about_content" : "<p>드론의 비행계획을 쉽게 디자인하고 비행기록과 상황를 확인할 수 있는 곳입니다.</p>
-																	<p>디자인 한 비행계획은 DUNI앱과 DJI제품의 드론에서 사용 할 수 있습니다. DUNI앱은 현재 개발중이며 곧 안드로이드/iOS버전이 배포될 예정입니다. ('비행계획' > '계획 설계' 메뉴)</p>
-																	<p>DUNI앱의 안드로이드 버전은 소스코드가 공개되어 있습니다. <a href='http://code.duni.io/' target=_new>이곳</a>에서 확인할 수 있습니다.</p>
-																	<p>DJI 비행기록 파일과 드론 영상이 있는 유튜브 URL을 업로드하면 자세한 비행기록을 확인할 수 있습니다. ('비행기록' > '기록 업로드' 메뉴)</p>
-																	<p>상단의 <i class='fa fa-user fa-fw'></i> 아이콘을 클릭하면 본인의 개발자 Token을 확인할 수 있습니다.</p>",
+				 "center_about_content" : "<p>드론의 비행계획을 쉽게 디자인하고 비행기록과 상황를 확인할 수 있는 곳입니다.</p>" +
+																	"<p>디자인 한 비행계획은 DUNI앱과 DJI제품의 드론에서 사용 할 수 있습니다. DUNI앱은 현재 개발중이며 곧 안드로이드/iOS버전이 배포될 예정입니다. ('비행계획' > '계획 설계' 메뉴)</p>" +
+																	"<p>DUNI앱의 안드로이드 버전은 소스코드가 공개되어 있습니다. <a href='http://code.duni.io/' target=_new>이곳</a>에서 확인할 수 있습니다.</p>" +
+																	"<p>DJI 비행기록 파일과 드론 영상이 있는 유튜브 URL을 업로드하면 자세한 비행기록을 확인할 수 있습니다. ('비행기록' > '기록 업로드' 메뉴)</p>" +
+																	"<p>상단의 <i class='fa fa-user fa-fw'></i> 아이콘을 클릭하면 본인의 개발자 Token을 확인할 수 있습니다.</p>",
  				 "msg_no_mission" : "There is no mission or something wrong.",
 				 "msg_error_sorry" : "Error. Sorry, please try again later.",
 				 "msg_no_flight_record" : "There is no flight record or something wrong."
