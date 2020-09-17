ad323b0449e0d3cb501503afcaa73a38c8992409
Gunwoo Lee <gunman@aply.biz>
2020-09-17 13:25:25 +0900

    3d 해제

--- center/js/center.js
+++ center/js/center.js
@@ -3351,8 +3351,8 @@ function addObjectTo3DMap(owner, kind) {
 
 function map3dInit() {
 	// ----[
-	$("#map3dViewer").hide();//for the license
-	return;
+	//$("#map3dViewer").hide();//for the license
+	//return;
 	// ---]
 
 	Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwMjRmOWRiNy1hMTgzLTQzNTItOWNlOS1lYjdmZDYxZWFkYmQiLCJpZCI6MzM1MTUsImlhdCI6MTU5ODg0NDIxMH0.EiuUUUoakHeGjRsUoLkAyNfQw0zXCk6Wlij2z9qh7m0';
