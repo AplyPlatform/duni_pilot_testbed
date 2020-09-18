06f823652f8a2208706de7477d0555b53ed4cfa9
gunman <gunman97@gmail.com>
2020-09-19 00:02:05 +0900

    dashboard 화면에 공개 데이터와 비교 차트 레이아웃 수정

--- center/js/center.js
+++ center/js/center.js
@@ -979,72 +979,10 @@ function setDashBoard(rcount, fcount, alltime, efcount, ealltime) {
 			    }		    		    		    
 		    ],
 		  },
-		  options: {
-		    maintainAspectRatio: false,
-		    layout: {
-		      padding: {
-		        left: 10,
-		        right: 25,
-		        top: 25,
-		        bottom: 0
-		      }
-		    },
-		    scales: {
-		      xAxes: [{
-		        time: {
-		          unit: 'data'
-		        },
-		        gridLines: {
-		          display: false,
-		          drawBorder: false
-		        },
-		        ticks: {
-		          maxTicksLimit: 6
-		        },
-		        maxBarThickness: 25,
-		      }],
-		      yAxes: [{
-		        ticks: {
-		          min: 0,
-		          max: 5000,
-		          maxTicksLimit: 5,
-		          padding: 10,
-		          // Include a dollar sign in the ticks
-		          callback: function(value, index, values) {
-		            return value;
-		          }
-		        },
-		        gridLines: {
-		          color: "rgb(234, 236, 244)",
-		          zeroLineColor: "rgb(234, 236, 244)",
-		          drawBorder: false,
-		          borderDash: [2],
-		          zeroLineBorderDash: [2]
-		        }
-		      }],
-		    },
+		  options: {		    
 		    legend: {
 		      display: false
-		    },
-		    tooltips: {
-		      titleMarginBottom: 10,
-		      titleFontColor: '#6e707e',
-		      titleFontSize: 14,
-		      backgroundColor: "rgb(255,255,255)",
-		      bodyFontColor: "#858796",
-		      borderColor: '#dddfeb',
-		      borderWidth: 1,
-		      xPadding: 15,
-		      yPadding: 15,
-		      displayColors: false,
-		      caretPadding: 10,
-		      callbacks: {
-		        label: function(tooltipItem, chart) {
-		          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-		          return datasetLabel + tooltipItem.yLabel;
-		        }
-		      }
-		    },
+		    }		    
 		  }
 		});
 		
