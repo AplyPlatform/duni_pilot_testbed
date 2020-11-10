ec0abdb7decbb24fd441a5bf3c9e16acef3bb536
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2020-11-10 16:51:01 +0900

    공개영상 표시 안되는 오류 수정중

--- center/js/center.js
+++ center/js/center.js
@@ -4973,6 +4973,17 @@ function addChartItem(i, item) {
 
 }
 
+function getQueryVariable(query, variable) {    
+  var varfirst = query.split('?');
+  var vars = varfirst[1].split('&');
+  for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+      if (decodeURIComponent(pair[0]) == variable) {
+          return decodeURIComponent(pair[1]);
+      }
+  }
+}
+	
 function getQueryVariable(variable) {
     var query = window.location.search.substring(1);
     var vars = query.split('&');
