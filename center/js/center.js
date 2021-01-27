50de65a3eb0efc33250712e02e026823187bc5ba
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2021-01-27 21:05:19 +0900

    [WIP] 로그인 후 첫화면에 유튜브 자동 재생 기능 추가 2

--- center/js/center.js
+++ center/js/center.js
@@ -69,7 +69,9 @@ var langset = "KR";
 var viewmode = "pilot"; // or "developer"
 var cur_controller;
 
-var use3DMap = true; 
+var use3DMap = true;
+
+var player = []; //youtube players 
 
 $(function () {
 
