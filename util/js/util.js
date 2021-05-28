57d31fd39502e7404df75a461a00f1dca3b656ee
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2021-05-28 14:25:10 +0900

    util 페이지의 지도 표시 오류 수정중

--- util/js/util.js
+++ util/js/util.js
@@ -621,16 +621,15 @@ function onPlayerStateChange(event) {
 
 	function moveFlightHistoryMapAndCada(lat, lng, cada) {
 		$("#historyMapArea").show();
-		alert("7");
-		return;
-		
 		var npos = ol.proj.fromLonLat([lng, lat]);
 		
-		/*
+		
 		container.style.opacity = 0.8;
 		var latlng = lat + "_" + lng;
 		overlayBoxcontent.innerHTML = "<div><h4>이 지역을 드론으로 촬영한<br>영상이 보고 싶지 않으세요?</h4><a class='btn btn-primary btn-sm' role='button' href='https://duni.io/index.php?page=rental' target='_new' onClick='GATAGM(\"util_request_duni_btn_1\",\"SERVICE\",\"" + latlng + "\",\"" + langset + "\");'>드론촬영 요청</a></div>";
-		*/
+		
+		alert("8");
+		return;
 		
 		overlayBox.setPosition(npos);
 		flightHistoryView.setCenter(npos);
