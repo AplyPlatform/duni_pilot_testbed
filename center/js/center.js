9ebb1004345c382bf2b448f95d43797d1e151f5f
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2021-06-17 23:36:25 +0900

    비행기록만 명시적으로 업로드 할 수 있도록 업로드 페이지 수정, 개발자/파일럿 페이지 전환 구조 개선, 비행기록 목록 지도 디자인 수정

--- center/js/center.js
+++ center/js/center.js
@@ -16,7 +16,8 @@ var g_str_current_target = "private";
 var g_str_cur_lang = "KR";
 var g_str_cur_viewmode = "pilot"; // or "developer"
 
-var g_str_cur_controller_for_viewmode;
+var g_str_pilot_controller_for_viewmode;
+var g_str_dev_controller_for_viewmode;
 
 var g_b_monitor_started;
 
@@ -93,6 +94,9 @@ var g_params_for_upload_flight_rec = {};
 
 var g_array_str_waypointactions_DJI = ["STAY", "START_TAKE_PHOTO", "START_RECORD", "STOP_RECORD", "ROTATE_AIRCRAFT", "GIMBAL_PITCH", "NONE", "CAMERA_ZOOM", "CAMERA_FOCUS"];
 
+var g_array_cur_controller_for_viewmode = { "pilot" : "/center/main.html", "developer" : "/center/main_dev.html" };		
+
+
 $(function () {
 		var lang = getCookie("language");
     if (isSet(lang))
@@ -157,8 +161,7 @@ function setViewMode() {
 
 	if(g_str_cur_viewmode == "") g_str_cur_viewmode = "pilot";
 
-	if(g_str_cur_viewmode == "pilot") {
-		g_str_cur_controller_for_viewmode = "/center/main.html";
+	if(g_str_cur_viewmode == "pilot") {				
 		$('#view_mode_selector').click(function(){
 			setCookie("viewmode", "developer", 1);
 			GATAGM('view_mode_selector_developer', 'MEMU');
@@ -166,7 +169,7 @@ function setViewMode() {
 		});
 	}
 	else {
-		g_str_cur_controller_for_viewmode = "/center/main_dev.html";
+		
 		$('#view_mode_selector').click(function(){
 			setCookie("viewmode", "pilot", 1);
 			GATAGM('view_mode_selector_pilot', 'MEMU');
@@ -764,6 +767,8 @@ function flightrecordUploadInit() {
 
     $("#tab_menu_set_youtube_address").text(GET_STRING_CONTENT('label_set_youtube_url'));
     $("#tab_menu_set_youtube_upload").text(GET_STRING_CONTENT('label_upload_movie'));
+    $("#tab_menu_set_no_video").text(GET_STRING_CONTENT('label_set_no_video'));
+    
 
     $("#flighttime_input_data_label").text(GET_STRING_CONTENT('flighttime_input_data_label'));
 
@@ -825,9 +830,13 @@ function flightrecordUploadInit() {
         	$("#set_youtube_address_view").show();
         	$("#set_youtube_upload_view").hide();
         }
-        else {
+        else if (cVal == "tab_menu_set_youtube_upload") {
         	$("#set_youtube_address_view").hide();
         	$("#set_youtube_upload_view").show();
+        }
+        else {
+        	$("#set_youtube_address_view").hide();
+        	$("#set_youtube_upload_view").hide();
         }
 		});
 
@@ -850,7 +859,7 @@ function flightrecordUploadInit() {
     	saveYoutubeUrl(g_params_for_upload_flight_rec, function(bSuccess) {
       	if (bSuccess == true) {
       		showAlert(GET_STRING_CONTENT('msg_success'));
-  				location.href = g_str_cur_controller_for_viewmode + "?page_action=recordlist";
+  				location.href = g_array_cur_controller_for_viewmode["pilot"] + "?page_action=recordlist";
       	}
       	else {
       		showAlert(GET_STRING_CONTENT('msg_error_sorry'));
@@ -901,7 +910,7 @@ function makeDateTimeFormat(d, isGMT) {
 function uploadCheckBeforeUploadFlightList() {
 
 		var cVal = $(":input:radio[name='media_upload_kind']:checked").val();
-		if (cVal == "tab_menu_set_youtube_address") {
+		if (cVal == "tab_menu_set_youtube_address" || cVal == "tab_menu_set_no_video") {
       uploadFlightList(false);
       return;
     }
@@ -1322,12 +1331,12 @@ function flightRecords2DMapInit() {
 				    		style = [new ol.style.Style({
 	                image: new ol.style.Circle({
 				            radius: radius,
-				            fill: new ol.style.Fill({ color: '#779977dd' }),
-				            stroke: new ol.style.Stroke({ color: '#ffffff', width: 2 })
+				            fill: new ol.style.Fill({ color: '#FFF' }),
+				            stroke: new ol.style.Stroke({ color: '#45cdba', width: 2 })
 		                }),
 		              text: new ol.style.Text({
 					                  text: size.toString(),
-					                  fill: new ol.style.Fill({ color: '#fff' }),
+					                  fill: new ol.style.Fill({ color: '#000' }),
 					                  scale: 1.5
 										})
 
@@ -1366,12 +1375,12 @@ function flightRecords2DMapInit() {
 			       style = [new ol.style.Style({
                 image: new ol.style.Circle({
 			            radius: radius,
-			            fill: new ol.style.Fill({ color: '#a03e8bd2' }),
-			            stroke: new ol.style.Stroke({ color: '#ffffff', width: 2 })
+			            fill: new ol.style.Fill({ color: '#FFF' }),
+			            stroke: new ol.style.Stroke({ color: '#fb5b58', width: 2 })
 	                }),
 	              text: new ol.style.Text({
 				                  text: size.toString(),
-				                  fill: new ol.style.Fill({ color: '#fff' }),
+				                  fill: new ol.style.Fill({ color: '#000' }),
 				                  scale: 1.5
 								})
               })];
@@ -2686,12 +2695,12 @@ function appendMissionList(data) {
 
     data.forEach(function (item, index, array) {
         var appendRow = "<div class='card shadow mb-4' id='mission_row_" + index + "'><div class='card-body'><div class='row'><div class='col-sm'>"
-            + "<a href='" + g_str_cur_controller_for_viewmode + "?page_action=missiondesign&mission_name=" + encodeURIComponent(item['name']) + "' class='font-weight-bold mb-1'>"
+            + "<a href='" + g_array_cur_controller_for_viewmode["developer"] + "?page_action=missiondesign&mission_name=" + encodeURIComponent(item['name']) + "' class='font-weight-bold mb-1'>"
             + item['name']
             + "</a></div></div><div class='row'><div class='col-sm text-xs font-weight-bold mb-1'>"
             + item['regtime']
             + "</div><div class='col-sm text-xs font-weight-bold mb-1'>"
-            + "<a class='btn btn-warning text-xs' href='" + g_str_cur_controller_for_viewmode + "?page_action=missiondesign&mission_name=" + encodeURIComponent(item['name']) + "' role='button'>" + GET_STRING_CONTENT('msg_modify') + "</a>&nbsp;"
+            + "<a class='btn btn-warning text-xs' href='" + g_array_cur_controller_for_viewmode["developer"] + "?page_action=missiondesign&mission_name=" + encodeURIComponent(item['name']) + "' role='button'>" + GET_STRING_CONTENT('msg_modify') + "</a>&nbsp;"
             + "<button class='btn btn-primary text-xs' type='button' id='missionListBtnForRemove_" + index + "'>"
             + GET_STRING_CONTENT('msg_remove') + "</button></div></div></div></div>";
         $('#dataTable-missions').append(appendRow);
@@ -3170,7 +3179,7 @@ function showDataWithName(target, target_key, name) {
 				    	var tagArray = JSON.parse(fdata.tag_values);
 				    	var appendRow = "";
 				    	tagArray.forEach(function(tg) {
-				    		appendRow = appendRow + "<a href=" + g_str_cur_controller_for_viewmode + "?page_action=" + targetList + "recordlist&keyword=" + encodeURIComponent(tg.value) + "><span class='badge badge-light'>" + tg.value + "</span></a> ";
+				    		appendRow = appendRow + "<a href=" + g_array_cur_controller_for_viewmode["pilot"] + "?page_action=" + targetList + "recordlist&keyword=" + encodeURIComponent(tg.value) + "><span class='badge badge-light'>" + tg.value + "</span></a> ";
 				    	});
 
 				    	$("#tagArrayarea").html(appendRow);
@@ -3599,11 +3608,11 @@ function appendFlightRecordTable(target, target_key, item) {
     if (target == "public") {
         appendRow = appendRow
             + "<a onclick='GATAGM(\"flight_list_public_title_click_"
-            + name + "\", \"CONTENT\");' href='" + g_str_cur_controller_for_viewmode + "?target_key=" + target_key + "&page_action=publicrecordlist_detail&record_name="
+            + name + "\", \"CONTENT\");' href='" + g_array_cur_controller_for_viewmode["pilot"] + "?target_key=" + target_key + "&page_action=publicrecordlist_detail&record_name="
             + encodeURIComponent(name) + "'>" + name + "</a>";
     }
     else {
-        appendRow = appendRow + "<a onclick='GATAGM(\"flight_list_title_click_" + name + "\", \"CONTENT\");' href='" + g_str_cur_controller_for_viewmode + "?target_key=" + target_key + "&page_action=recordlist_detail&record_name="
+        appendRow = appendRow + "<a onclick='GATAGM(\"flight_list_title_click_" + name + "\", \"CONTENT\");' href='" + g_array_cur_controller_for_viewmode["pilot"] + "?target_key=" + target_key + "&page_action=recordlist_detail&record_name="
             + encodeURIComponent(name) + "'>" + name + "</a>";
     }
 
@@ -3636,7 +3645,7 @@ function appendFlightRecordTable(target, target_key, item) {
     	var targetList = (target == "public" ? "public" : "");
     	var tag_array = JSON.parse(tag_values);
     	tag_array.forEach(function(tg) {
-    		appendRow = appendRow + "<a href=" + g_str_cur_controller_for_viewmode + "?page_action=" + targetList + "recordlist&keyword=" + encodeURIComponent(tg.value) + "><span class='badge badge-light'>" + tg.value + "</span></a> ";
+    		appendRow = appendRow + "<a href=" + g_array_cur_controller_for_viewmode["pilot"] + "?page_action=" + targetList + "recordlist&keyword=" + encodeURIComponent(tg.value) + "><span class='badge badge-light'>" + tg.value + "</span></a> ";
     	});
     }
 
@@ -3661,7 +3670,7 @@ function appendFlightRecordTable(target, target_key, item) {
 
     if (target == "public") {
         if (isSet(owner_email)) {
-            var oemail = "<a href='" + g_str_cur_controller_for_viewmode + "?page_action=publicrecordlist&user_email=" + encodeURIComponent(owner_email) + "'>" + owner_email + "</a>";
+            var oemail = "<a href='" + g_array_cur_controller_for_viewmode["pilot"] + "?page_action=publicrecordlist&user_email=" + encodeURIComponent(owner_email) + "'>" + owner_email + "</a>";
             $("#owner_email_" + curIndex).show();
             $("#owner_email_" + curIndex).html(oemail);
         }
@@ -3847,7 +3856,7 @@ function deleteFlightData(name, index) {
             	removeTableRow("flight-list-" + index);
             else {
             	alert(GET_STRING_CONTENT('msg_success'));
-            	location.href = g_str_cur_controller_for_viewmode + "?page_action=recordlist";
+            	location.href = g_array_cur_controller_for_viewmode["pilot"] + "?page_action=recordlist";
             }
         }
     }, function (request, status, error) {
@@ -3960,7 +3969,7 @@ function registMission(mname, mspeed) {
     ajaxRequest(jdata, function (r) {
         if (r.result == "success") {
 				    alert(mname + " (" + mspeed + "m/s) : " + GET_STRING_CONTENT('msg_success'));
-				    location.href = g_str_cur_controller_for_viewmode + "?page_action=missionlist";
+				    location.href = g_array_cur_controller_for_viewmode["developer"] + "?page_action=missionlist";
         }
         else {
             showAlert(GET_STRING_CONTENT('msg_error_sorry'));
@@ -4802,6 +4811,11 @@ function uploadFlightList(isUpdate) {
 
 		var youtube_data = $("#youtube_url_data").val();
     var files = document.getElementById('flight_record_file').files;
+    
+    var cVal = $(":input:radio[name='media_upload_kind']:checked").val();
+		if (cVal == "tab_menu_set_no_video") {
+      youtube_data = "";
+    }
 
     var params = {};
 
@@ -4879,7 +4893,7 @@ function uploadFlightList(isUpdate) {
     	saveYoutubeUrl(params, function(bSuccess) {
         	if (bSuccess == true) {
         		showAlert(GET_STRING_CONTENT('msg_success'));
-        		location.href = g_str_cur_controller_for_viewmode + "?page_action=recordlist";
+        		location.href = g_array_cur_controller_for_viewmode["pilot"] + "?page_action=recordlist";
         	}
         	else {
         		showAlert(GET_STRING_CONTENT('msg_error_sorry'));
@@ -4924,7 +4938,7 @@ function uploadDJIFlightListCallback(params) {
             $('#uploadFileform').hide(1500);
             GATAGM('dji_file_upload_success', 'CONTENT');
             alert(GET_STRING_CONTENT('msg_success'));
-            location.href = g_str_cur_controller_for_viewmode + "?page_action=recordlist";
+            location.href = g_array_cur_controller_for_viewmode["pilot"] + "?page_action=recordlist";
         }
         else {
             if (r.result_code == 3) {
@@ -5197,7 +5211,7 @@ function setFlightRecordTitleName() {
         if (r.result == "success") {
 					showAlert(GET_STRING_CONTENT('msg_success'));
 					g_str_cur_flight_record_name = target_name;
-					location.href = g_str_cur_controller_for_viewmode + "?page_action=recordlist_detail&record_name=" + encodeURIComponent(target_name);
+					location.href = g_array_cur_controller_for_viewmode["pilot"] + "?page_action=recordlist_detail&record_name=" + encodeURIComponent(target_name);
         }
         else {
         	if (r.result_code == 3)
@@ -5308,14 +5322,25 @@ function setYoutubePlayerForDetaileView(d_id) {
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 }
 
-function onYouTubeIframeAPIReady() {
-		if (g_str_cur_viewmode != "pilot") return;
-
-		if (g_str_page_action == "recordlist" || g_str_page_action == "publicrecordlist" || g_str_page_action == "center" || g_str_page_action == "summary_list") {
+function onYouTubeIframeAPIReady() {		
+		if (g_str_cur_viewmode == "pilot" &&
+				(g_str_page_action == "recordlist" 
+				|| g_str_page_action == "publicrecordlist" 
+				|| g_str_page_action == "center" 
+				|| g_str_page_action == "summary_list")) {
 			getFullFlightRecords(g_str_current_target);
     	getFlightRecords(g_str_current_target);
     	return;
     }
+    
+    if (g_str_cur_viewmode != "pilot"
+    		&& g_str_page_action == "center" ) {
+    	getFullFlightRecords(g_str_current_target);
+    	getFlightRecords(g_str_current_target);
+    	return;
+    }
+    
+    if (g_str_cur_viewmode != "pilot") return;
 
     g_youtube_player_for_detail_view = new YT.Player('youTubePlayer', {
         width: '1000',
