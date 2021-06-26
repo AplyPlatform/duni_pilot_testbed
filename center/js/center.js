cf69860ea7270f35caabd3a0a836c6a0db7131f4
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2021-06-26 16:28:16 +0900

    영상에 나침반 삽입 요청이 완료되면 업로드 버튼 비활성화

--- center/js/center.js
+++ center/js/center.js
@@ -1077,7 +1077,8 @@ function embedRequest(filename, tempExt) {
     ajaxRequest(jdata, function (r) {
         if (r.result == "success") {
         	setProgress(100); //전체 프로그레스바 진행
-        	showAlert("변환 요청이 접수 되었습니다. 처리가 완료되면 가입시 등록한 이메일(" + getCookie("user_email") + ")로 영상파일의 다운로드 경로를 알려 드립니다.");
+        	showAlert("변환 요청이 접수 되었습니다. 처리가 완료되면 가입시 등록한 이메일(" + getCookie("user_email") + ")로 영상파일의 다운로드 경로를 알려 드립니다.");        	
+        	$('#btnForUploadFlightList').prop('disabled', true);
         }
         else showAlert("failed - " + r.reason);
     }, function (request, status, error) {
