60073d02cc4bc1d17d71340a09f276a414326e32
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2021-06-28 14:01:12 +0900

    비행기록 목록의 촬영/등록 시각 표시 레이아웃 수정

--- center/js/center.js
+++ center/js/center.js
@@ -3807,14 +3807,14 @@ function appendFlightRecordTable(target, target_key, item) {
 
     appendRow = appendRow + "</div><div class='row'><div class='col-sm text-left'><span id='owner_email_" + curIndex + "' class='text-xs font-weight-bold mb-1'></span></div><div class='col-sm text-right'><a href='#' class='text-xs' id='map_address_" + curIndex + "'></a></div></div>";
 
-		appendRow = appendRow + "<div class='row'><div class='col-10 text-right'><br><textarea class='form-control' id='memoTextarea_" + curIndex + "' rows='1'>"
+		appendRow = appendRow + "<div class='row'><div class='col-md-10 text-right'><br><textarea class='form-control' id='memoTextarea_" + curIndex + "' rows='1'>"
 
     if (isSet(memo)) {
         appendRow = appendRow + memo;
     }
 
-    appendRow = appendRow + "</textarea></div><div class='col-2 text-right'>";
-    appendRow = appendRow + "<br><button class='btn btn-primary text-xs' type='button' id='btnForUpdateMemo_" + curIndex + "'>" + GET_STRING_CONTENT('msg_modify_memo') + "</button></div></div>"; //col-sm row
+    appendRow = appendRow + "</textarea></div><div class='col-md-2 text-right'>";
+    appendRow = appendRow + "<br><button class='btn btn-primary text-xs btn-block' type='button' id='btnForUpdateMemo_" + curIndex + "'>" + GET_STRING_CONTENT('msg_modify_memo') + "</button></div></div>"; //col-sm row
 
 
     appendRow = appendRow + "<div class='row'><div class='col-sm'>";
@@ -3828,16 +3828,16 @@ function appendFlightRecordTable(target, target_key, item) {
     }
 
     appendRow = appendRow + "</div></div>";
-    appendRow = appendRow + "<div class='row'>";
+    appendRow = appendRow + "<div class='row'><div class='col-md-12 text-left'>";
 
 
     if (isSet(item.starttime)) {
-    	appendRow = appendRow + "<div class='col-sm text-left'><span class='text-xs mb-1'>" + GET_STRING_CONTENT('flighttime_input_data_label') + "</span> <span class='text-xs mb-1'>"
+    	appendRow = appendRow + "<span class='text-xs mb-1'>" + GET_STRING_CONTENT('flighttime_input_data_label') + "</span> <span class='text-xs mb-1'>"
     							+ makeDateTimeFormat(new Date(item.starttime), true)
-    							+ "</span></div>";
+    							+ " / </span>";
     }
 
-    appendRow = appendRow + "<div class='col-sm text-right'><span class='text-xs mb-1'>" + GET_STRING_CONTENT('registered_datetime_label') + "</span> <span class='text-xs mb-1'>" + dtimestamp + "</span>";
+    appendRow = appendRow + "<span class='text-xs mb-1'>" + GET_STRING_CONTENT('registered_datetime_label') + "</span> <span class='text-xs mb-1'>" + dtimestamp + "</span>";
     appendRow = appendRow + "</div></div><div class='row'><div class='col-sm text-right'>"
         + "<h6><span class='badge badge-secondary' style='cursor: pointer' id='btnForRemoveFlightData_" + curIndex + "'>" + GET_STRING_CONTENT('msg_remove') + "</span></h6>"
 
