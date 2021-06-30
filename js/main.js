52bdeb613fec99a9dcd39b2a20094c1fbd1032e9
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2021-06-30 21:59:58 +0900

    [wip] 비행기록, 기업 이미지 수정

--- js/main.js
+++ js/main.js
@@ -572,10 +572,10 @@
 				    			style = [new ol.style.Style({
 		                image: new ol.style.Icon({										    										    
 										    src: '/images/company_pos.png',
-										    scale: 0.8,
-										    opacity : 0.7,
-										    stroke: new ol.style.Stroke({ color: '#45cdba', width: 2 }),
-										    fill: new ol.style.Fill({ color: '#fff' }),
+										    scale: 0.3,
+										    opacity : 0.7,										    
+										    fill: new ol.style.Fill({ color: '#545e83' }),
+										    stroke: new ol.style.Stroke({ color: '#666666', width: 2 }),
 										  })
 		              })];
 				    		}
@@ -584,12 +584,12 @@
 		                image: new ol.style.Circle({
 					            radius: radius,
 					            opacity : 0.7,
-					            fill: new ol.style.Fill({ color: '#FFF' }),
-					            stroke: new ol.style.Stroke({ color: '#45cdba', width: 2 })
+					            fill: new ol.style.Fill({ color: '#545e83' }),
+					            stroke: new ol.style.Stroke({ color: '#666666', width: 2 })
 			                }),
 			            	text: new ol.style.Text({
 						                  text: size.toString(),
-						                  fill: new ol.style.Fill({ color: '#000' }),
+						                  fill: new ol.style.Fill({ color: '#fff' }),
 						                  scale: 1.5
 											})	
 		              })];
@@ -628,7 +628,7 @@
 				       		style = [new ol.style.Style({
 		                image: new ol.style.Icon({										    										    
 										    src: '/images/f_record_pos.png',
-										    scale: 0.8,
+										    scale: 0.3,
 										    opacity : 0.7,
 										    stroke: new ol.style.Stroke({ color: '#FB5B58', width: 2 }),
 										    fill: new ol.style.Fill({ color: '#fff' }),
@@ -639,13 +639,13 @@
 					    		style = [new ol.style.Style({
 		                image: new ol.style.Circle({
 					            radius: radius,
-					            fill: new ol.style.Fill({ color: '#FFF' }),
-					            stroke: new ol.style.Stroke({ color: '#FB5B58', width: 2 }),
+					            fill: new ol.style.Fill({ color: '#495057' }),
+					            stroke: new ol.style.Stroke({ color: '#666666', width: 2 }),
 					            opacity : 0.7,
 			                }),
 			            	text: new ol.style.Text({
 						                  text: size.toString(),
-						                  fill: new ol.style.Fill({ color: '#000' }),
+						                  fill: new ol.style.Fill({ color: '#fff' }),
 						                  scale: 1.5
 											})	
 		              })];
