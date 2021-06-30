88b3e8c1e0e8ff01f8f6dce63c12c3305e7f772d
Hyeji Kim <hyejikim95@github.com>
2021-06-30 16:37:07 +0900

    msg name bug fix

--- center/js/lang.js
+++ center/js/lang.js
@@ -103,7 +103,7 @@ var LANG_JSON_DATA = {
         // 전화번호인증 end
         // 이메일 중복확인
         "msg_email_empty" : "이메일을 입력해주세요",
-        "msg_invalid_email" : "올바른 이메일 형식이 아닙니다. 이메일은 100자 이내여야합니다.",
+        "msg_email_valid" : "올바른 이메일 형식이 아닙니다. 이메일은 100자 이내여야합니다.",
         "msg_email_already_exists" : "이미 사용중인 이메일입니다. 다른 이메일을 입력해주세요.",
         "msg_email_valid" : "사용 가능한 이메일입니다.",
         "msg_email_not_verified" : "이메일 중복확인은 필수입니다. 중복확인을 해주세요.",
@@ -336,7 +336,7 @@ var LANG_JSON_DATA = {
         // 전화번호인증 end
         // 이메일 중복확인
         "msg_email_empty" : "Please enter your email address.",
-        "msg_invalid_email" : "Incorrect email address format. Please try again.",
+        "msg_email_invalid" : "Incorrect email address format. Please try again.",
         "msg_email_already_exists" : "Email already exists. Try a different address.",
         "msg_email_valid" : "Valid email.",
         "msg_email_not_verified" : "You must validate your email address.",
