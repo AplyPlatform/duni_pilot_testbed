f6b7f74717ff4b570dbd6b12b918defc7839d0af
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2021-07-17 23:00:52 +0900

    [WIP] 나침반 영상 선택하자마자 샘플 영상 보이는 기능 추가6

--- center/js/center.js
+++ center/js/center.js
@@ -1,4 +1,6 @@
-﻿/*
+﻿"use strict";
+
+/*
 Copyright 2021 APLY Inc. All rights reserved.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -10,8 +12,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
-
 
 var g_b_monitor_started;
 var g_b_phonenumber_verified = true;
