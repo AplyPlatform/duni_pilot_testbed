06fd03a28073037335c43b45af4704dd3755d0b7
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2021-07-25 15:16:51 +0900

    비행기록 업로드 할 때 유튜브주소 입력 클릭하면 기본 설정 반영 되게끔 개선2

--- center/js/center.js
+++ center/js/center.js
@@ -901,8 +901,8 @@ function flightrecordUploadInit() {
 			GATAGM('label_youtube_address_only', 'CONTENT');
 			$("#nextStageBtnArea").hide();
 			$("#set_youtube_address_view").show();
-      $("#set_youtube_upload_view").hide();
-      $('#tab_menu_set_youtube_address').attr("checked", true);
+      $("#set_youtube_upload_view").hide();      
+      $('input:radio[name=media_upload_kind]:input[value=tab_menu_set_youtube_address]').attr("checked", true);
 			setUploadFileFields();
 		});
 
