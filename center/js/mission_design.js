c001d20bd0757a350b061f5e9f905d3612c5c73f
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2022-02-17 17:21:56 +0900

    테스트 코드 추가

--- center/js/mission_design.js
+++ center/js/mission_design.js
@@ -1,15 +1,4 @@
-﻿/*
-Copyright 2015 Google Inc. All Rights Reserved.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-  http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+﻿/* Copyright 2022 APLY Inc. All rights reserved. */
 
 "use strict";
 $(function () {
@@ -18,6 +7,8 @@ $(function () {
 
 function designInit() {
     debugger;
+
+    alert("design Init is called!!");
     map2DInit();
     selectMonitorIndex("private", 0);
     addObjectTo2DMap(0, "private", "drone");
